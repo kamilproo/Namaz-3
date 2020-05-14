25da7adb607265b4ebe1baeeae6aa4928aa9abd9
kamil <kamilproo@yandex.ru>
2020-05-14 08:35:33 +0300

    Initial Commit

---
+++ Namaz/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Namaz
+//
+//  Created by Камиль Зиязетдинов on 14.05.2020.
+//  Copyright © 2020 Камиль Зиязетдинов. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
